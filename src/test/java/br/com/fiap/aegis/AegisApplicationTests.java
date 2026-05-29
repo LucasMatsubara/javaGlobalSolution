61a56c5c941162ee9eb654011267c505faf8beff
LucasMatsubara <lucasmatsubara11@gmail.com>
2026-05-28 22:16:26 -0300

    Inicializando o projeto.

---
+++ src/test/java/br/com/fiap/aegis/AegisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.aegis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AegisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
