7fff225966cab21e2bf9aef3da6b8bb6ccc241b5
LucasMatsubara <lucasmatsubara11@gmail.com>
2026-06-03 23:30:32 -0300

    Adaptando DTOs referente ao satélite..

--- src/main/java/br/com/fiap/aegis/dto/SateliteRequestDTO.java
+++ src/main/java/br/com/fiap/aegis/dto/SateliteRequestDTO.java
@@ -1,15 +1,21 @@
 package br.com.fiap.aegis.dto;
 
-import br.com.fiap.aegis.enums.TipoBanda;
+import br.com.fiap.aegis.enums.StatusSatelite;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.Valid;
 
+import java.time.LocalDate;
+
 public record SateliteRequestDTO(
         @NotBlank(message = "O nome do satélite é obrigatório") String nome,
+        @NotBlank(message = "O NORAD ID é obrigatório") String noradId,
+        @NotNull(message = "A inclinação é obrigatória") Double inclinacao,
+        @NotNull(message = "A data de lançamento é obrigatória") LocalDate dataLancamento,
+        @NotNull(message = "O status do satélite é obrigatório") StatusSatelite statusSatelite,
         @NotNull(message = "A massa é obrigatória") @Positive Double massaKg,
         @NotNull(message = "As coordenadas são obrigatórias") @Valid CoordenadaDTO coordenadas,
-        @NotNull(message = "O tipo de banda é obrigatório") TipoBanda tipoBanda,
+        @NotNull(message = "O tipo de banda é obrigatório") String tipoBanda,
         @NotNull(message = "O ID da empresa proprietária é obrigatório") Long empresaId
 ) {}
--- src/main/java/br/com/fiap/aegis/dto/SateliteResponseDTO.java
+++ src/main/java/br/com/fiap/aegis/dto/SateliteResponseDTO.java
@@ -1,12 +1,18 @@
 package br.com.fiap.aegis.dto;
 
-import br.com.fiap.aegis.enums.TipoBanda;
+import br.com.fiap.aegis.enums.StatusSatelite;
+
+import java.time.LocalDate;
 
 public record SateliteResponseDTO(
         Long id,
         String nome,
+        String noradId,
+        Double inclinacao,
+        LocalDate dataLancamento,
+        StatusSatelite statusSatelite,
         Double massaKg,
+        String tipoBanda,
         CoordenadaDTO coordenadas,
-        TipoBanda tipoBanda,
         EmpresaResponseDTO empresa
 ) {}
